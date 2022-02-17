35e9e9a34eeb920d7a07d5b4314f7082fa394813
Boni Machado <bonissauro@gmail.com>
2022-02-17 19:33:39 -0300

    Ajutes para exibicao de mensagem para lista vazia

--- App.js
+++ App.js
@@ -22,31 +22,31 @@ export default function App() {
   const [activeIndex, setActiveIndex] = useState(0);
   const [lista, setLista] = useState(
     [
-      {
-        id: "1",
-        nome: "Tomate",
-        url: "https://st.depositphotos.com/1063437/1966/i/600/depositphotos_19668189-stock-photo-raw-vegetables-in-wicker-basket.jpg"
-      },
-      {
-        id: "2",
-        nome: "Brócolis",
-        url:"https://static.significados.com.br/foto/verduras-e-legumes-fb.jpg"
-      },
-      {
-        id: "3",
-        nome: "Batata",
-        url:"https://conteudo.imguol.com.br/c/entretenimento/fa/2019/04/18/legumes-e-verduras-1555620381375_v2_615x300.jpg"
-      },
-      {
-        id: "4",
-        nome: "Pepino",
-        url:"https://i.pinimg.com/736x/9f/d9/b7/9fd9b7f8247318207fe3115b01320409.jpg"
-      },
-      {
-        id: "5",
-        nome: "Abóbora",
-        url:"https://a-static.mlcdn.com.br/1500x1500/1200-sementes-verduras-e-legumes-30-tipos-separados-kit-surpresa-isla-sementes/semearshop/14c81a42e40e11eb81eb4201ac18500e/c67c919c3eec962c703f5ae1a48c2524.jpg"
-      },
+      // {
+      //   id: "1",
+      //   nome: "Tomate",
+      //   url: "https://st.depositphotos.com/1063437/1966/i/600/depositphotos_19668189-stock-photo-raw-vegetables-in-wicker-basket.jpg"
+      // },
+      // {
+      //   id: "2",
+      //   nome: "Brócolis",
+      //   url:"https://static.significados.com.br/foto/verduras-e-legumes-fb.jpg"
+      // },
+      // {
+      //   id: "3",
+      //   nome: "Batata",
+      //   url:"https://conteudo.imguol.com.br/c/entretenimento/fa/2019/04/18/legumes-e-verduras-1555620381375_v2_615x300.jpg"
+      // },
+      // {
+      //   id: "4",
+      //   nome: "Pepino",
+      //   url:"https://i.pinimg.com/736x/9f/d9/b7/9fd9b7f8247318207fe3115b01320409.jpg"
+      // },
+      // {
+      //   id: "5",
+      //   nome: "Abóbora",
+      //   url:"https://a-static.mlcdn.com.br/1500x1500/1200-sementes-verduras-e-legumes-30-tipos-separados-kit-surpresa-isla-sementes/semearshop/14c81a42e40e11eb81eb4201ac18500e/c67c919c3eec962c703f5ae1a48c2524.jpg"
+      // },
   ]
   );
     
@@ -136,6 +136,32 @@ export default function App() {
 
   } 
   
+  function Corpo(props) {
+
+    if (lista.length==0){
+      
+      return <View style={{flex: 1, alignItems:  "center",  paddingTop: 250}}>
+        <Text style={{fontSize: 24, fontWeight: "bold", textAlign: "center"}}>AINDA NAO HÁ FOTOS NO SEU ÁLBUM!</Text>
+      </View>
+
+    }else{
+
+      return <Carousel
+              ref={ref => this.carousel = ref}
+              data={lista}
+              sliderWidth={larguraTela}
+              itemWidth={larguraTela}
+              renderItem={_renderizaSlide}
+              style={{backgroundColor: "red"}}
+              loop={ true }
+              onSnapToItem = { (index) => {
+                setActiveIndex( index ); 
+                } 
+              } />;
+    }
+
+  }
+
   if (!cameraAberta){
 
   return (<SafeAreaView style={{flex: 1}}>
@@ -143,22 +169,7 @@ export default function App() {
 
         <Text onPress={listar} style={{padding: 30, fontSize: 32, fontWeight: "bold", alignItems: "flex-start"}}>{(cameraAberta)?"Câmera aberta":"App Teste"}</Text>
 
-        <View>
-          <>
-          <Carousel
-            ref={ref => this.carousel = ref}
-            data={lista}
-            sliderWidth={larguraTela}
-            itemWidth={larguraTela}
-            renderItem={_renderizaSlide}
-            style={{backgroundColor: "red"}}
-            loop={ true }
-            onSnapToItem = { (index) => {
-              setActiveIndex( index ); 
-              } 
-            } />
-          </>
-        </View>
+        <Corpo />
 
         <Text
               onPress={() => {     
